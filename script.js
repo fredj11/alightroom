793aa936593662881d38be8232577dd070d2eabc
Frederic Jarjour <fredj.happy@gmail.com>
2021-11-08 16:49:30 -0500

    it no work

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = 0;
+window.addEventListener('DOMContentLoaded', (event) => {
+    var button = document.getElementById('btn');
+});
+
+if(button.clicked == true)
+{
+    a++;
+    console.log(a);
+}
