0e521e7b69493e6b4f8ed4ab5f839ca5d8c3c3aa
Frederic Jarjour <fredj.happy@gmail.com>
2021-11-09 09:45:13 -0500

    resource system, bug in the timers

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
 var population = 5;
 var delay = 0;
-var cash = 0;
 
-function addWorker()
+resources = ["wood", "stone", "food"];
+
+resourceAmounts = {} // resource: amount of resource owned
+
+timers = {}; // resource: timerID of resource
+
+popAssigned = {}; // resource: population assigned to resource
+
+for (var i = 0; i < resources.length; i++) {
+    resourceAmounts[resources[i]] = 0;
+    popAssigned[resources[i]] = 0;
+}
+
+function addWorker(resource)
 {
     if (population > 0) {
         delay++;
-        stopTimer();
-        startTimer();
+        stopTimer(resource);
+        startTimer(resource);
         population--;
-        updatePop();
+        popAssigned[resource]++;
+        updateText();
     }
 }
 
-function removeWorker() {
-    if (population < 5) {
+function removeWorker(resource) {
+    if (population < 5 && popAssigned[resource] > 0) {
         delay--;
-        stopTimer();
-        startTimer();
+        stopTimer(resource);
+        startTimer(resource);
         population++;
-        updatePop();
+        popAssigned[resource]--;
+        updateText();
     }
 }
 
-function updatePop() {
+function updateText() {
     var pop = document.getElementById("population");
     pop.innerHTML = "population: " + population;
+
+    for (var i = 0; i < resources.length; i++) {
+        var res = resources[i];
+        var resourceText = document.getElementById(res);
+        resourceText.innerHTML = res + ": " + resourceAmounts[res] + " (" + popAssigned[res] + " workers)";
+    }
+
 }
 
-function addNumber() {
-    var number = document.getElementById("number");
-    number.innerHTML = "money: " + cash++;
+function addNumber(resource) {
+    var number = document.getElementById(resource);
+    console.log("adding " + resource);
+    resourceAmounts[resource]++;
+    number.innerHTML = resource + ": " + resourceAmounts[resource] + " (" + popAssigned[resource] + " workers)";
 }
 
-var intervalID;
-function startTimer() {
-    if (delay > 0)
-        intervalID = setInterval(addNumber, 1000/delay);
+function startTimer(resource) {
+    if (delay > 0) {
+        var timerID = setInterval(function() { addNumber(resource)}, 1000/delay);
+        timers[resource] = timerID;
+    } 
 }
 
-function stopTimer() {
-    clearInterval(intervalID);
+function stopTimer(resource) {
+    clearInterval(timers[resource]);
 }
