8d19f1b8e40e192e364f7895bad54fbbe52fad19
Frederic Jarjour <fredj.happy@gmail.com>
2021-11-08 21:05:57 -0500

    population mechanic

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-var delay = 1;
+var population = 5;
+var delay = 0;
 var cash = 0;
 
-function buttonClick()
+function addWorker()
 {
-    delay++;
-    stopTimer();
-    startTimer();
+    if (population > 0) {
+        delay++;
+        stopTimer();
+        startTimer();
+        population--;
+        updatePop();
+    }
+}
+
+function removeWorker() {
+    if (population < 5) {
+        delay--;
+        stopTimer();
+        startTimer();
+        population++;
+        updatePop();
+    }
+}
+
+function updatePop() {
+    var pop = document.getElementById("population");
+    pop.innerHTML = "population: " + population;
 }
 
 function addNumber() {
     var number = document.getElementById("number");
-    number.innerHTML = cash++;
-    console.log(cash);
+    number.innerHTML = "money: " + cash++;
 }
 
 var intervalID;
 function startTimer() {
-    intervalID = setInterval(addNumber, 1000/delay);
-    console.log("interval started")
+    if (delay > 0)
+        intervalID = setInterval(addNumber, 1000/delay);
 }
 
 function stopTimer() {
